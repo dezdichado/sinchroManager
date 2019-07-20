e1bcbbbf9e067155fa781b6c6d3fabcf05ec1fa9
dezdichado <mister-proper-veselo@yandex.ru>
2019-07-20 20:21:44 +0200

    added create_poll function

--- api.py
+++ api.py
@@ -22,13 +22,35 @@ def call_method(name, params):
     response = urllib.request.urlopen(url, context=ctx).read().decode()
     return response
 
-def send_text(text):
+
+def send_message(text, attachments=None):
     method = "messages.send"
     params = {
         "peer_id": config.DIFFICHENTO_ID,
         "message": text,
         "random_id": time.time()
     }
+    if attachments is not None:
+        params["attachment"] = ','.join(attachments)
 
     return call_method(method, params)
 
+def create_poll(question, answers, is_anonymous=0, is_multiple=1, end_date=-1,
+                background_id=1):
+    '''
+    :param question: str
+    :param answers: list
+    :return: str
+    '''
+    method = "polls.create"
+    params = {
+        "question": question,
+        "add_answers": str(answers),
+        "is_anonymous": is_anonymous,
+        "is_multiple": is_multiple,
+        "background_id": background_id
+    }
+    if end_date >= 1558003357:
+        params["end_date"] = end_date
+
+    return call_method(method, params)
