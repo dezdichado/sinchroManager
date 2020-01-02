2c5b726dc36b3657fe560c3513060e3a224d4c47
dezdichado <mister-proper-veselo@yandex.ru>
2020-01-03 03:35:23 +0500

    changed offset parameter

--- parseVK.py
+++ parseVK.py
@@ -5,7 +5,6 @@ import datetime
 
 
 def get_latest_post():
-    temp_offset = (int(datetime.datetime.timestamp(datetime.datetime.now())) // 60) % 30
-    posts = api.search_wall(config.CHGK_VK_ID, config.WEEKEND_HASHTAG, count=1, offset=temp_offset)
+    posts = api.search_wall(config.CHGK_VK_ID, config.WEEKEND_HASHTAG, count=1, offset=0
     posts = json.loads(posts)
     return posts["response"]["items"][0]
